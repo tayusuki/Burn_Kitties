aeca56de3e0ea6cee98c040b72a4f33697e928bd
tayusuki <aestrada2@mail.pima.edu>
2018-12-01 23:55:43 -0700

    Sample Scene for Alena with flexible textbox

---
+++ Assets/Script/Dialogue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour {
+
+    //attach to player
+
+    public List<string> random = new List<string>();
+    public List<string> whenCatsAreSacrificed = new List<string>();
+    public List<string> victory = new List<string>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/TextboxBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextboxBehavior : MonoBehaviour {
+
+    Text textbox;
+    string text;
+
+
+	void Start () {
+        textbox = GetComponent<Text>();
+	}
+	
+	void Update () {
+		
+	}
+
+    void SaySomething()
+    {
+        //enable textbox
+        //animate text (stop all coroutines, in case another event triggers a box)
+        //set timer to disable the box after all text is visible
+    }
+}
