35f4b2b02a5b979ea3783689f4b314e1b8eca993
DigiGee <byongbanthom@yahoo.com>
2018-12-02 03:48:57 -0700

    Cat throwing in

--- Assets/Script/CharacterController2d.cs
+++ Assets/Script/CharacterController2d.cs
@@ -9,7 +9,7 @@ public class CharacterController2d : MonoBehaviour {
 	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
+	public LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	public Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	public Transform m_CeilingCheck;							// A position marking where to check for ceilings
 
@@ -17,7 +17,7 @@ public class CharacterController2d : MonoBehaviour {
 	public bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	internal bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
 
 	[Header("Events")]
--- Assets/Script/PlayerPickup.cs
+++ Assets/Script/PlayerPickup.cs
@@ -4,13 +4,125 @@ using UnityEngine;
 
 public class PlayerPickup : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject catVisPrefab;
+	public List<GameObject> heldCats;
+	public List<GameObject> storedCats;
+
+	public float catHeight = 0.5f;
+	public float catTowerHeight = 1.5f;
+	Coroutine catAddingInstance;
+
+	public LayerMask ignoreCheck;
+	[Range(0,1)] public float maxLean = 0.5f;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("Fire1")) {
+			if (catAddingInstance == null && FindClosestGato() != null) {
+				catAddingInstance = StartCoroutine (AddGato (FindClosestGato()));
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.V)) {
+			//RemoveTopGato ();
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (heldCats.Count > 0 && catAddingInstance == null) {
+				StartCoroutine(ThrowGato());
+			}
+		}
+	}
+
+	GameObject FindClosestGato(){
+		GameObject closeCat = null;
+		foreach(Collider2D hit in Physics2D.OverlapCircleAll(GetComponent<CharacterController2d>().transform.position, 0.5f)){
+			float bestdist = Mathf.Infinity;
+			if (hit.tag == "Cat") {
+				if (Vector2.Distance (GetComponent<CharacterController2d>().m_GroundCheck.position, hit.transform.position) < bestdist) {
+					closeCat = hit.gameObject;
+				}
+			}
+		}
+		Debug.Log (closeCat);
+		return closeCat;
+	}
+
+	IEnumerator AddGato(GameObject targetGato){
+		storedCats.Add (targetGato);
+		targetGato.SetActive(false);
+		yield return new WaitForEndOfFrame ();
+		 
+		if (Physics2D.OverlapCircleAll(new Vector2 (transform.position.x, transform.position.y + catTowerHeight + catHeight), 0.25f, ignoreCheck).Length != 0 && heldCats.Count > 0) {
+			RemoveTopGato ();
+		}
+
+		heldCats.Add(Instantiate(catVisPrefab));
+		heldCats[heldCats.Count - 1].transform.parent = this.transform;
+		catTowerHeight += catHeight;
+		float elapsedTime = 0f;
+		while (elapsedTime < 1f) {
+			for (int i = 0; i < heldCats.Count; i++) {
+				heldCats [i].transform.localPosition = Vector2.Lerp (new Vector2 (0f, catTowerHeight - (catHeight * i)), new Vector2 (0f, catTowerHeight - (catHeight * (i - 1))), elapsedTime); 
+				elapsedTime += Time.deltaTime * 4f;
+				yield return new WaitForEndOfFrame ();
+			}
+		}
+		catAddingInstance = null;
+	}
+
+	IEnumerator ThrowGato(){
+		if (GetComponent<CharacterController2d> ().m_FacingRight) {
+			storedCats [storedCats.Count - 1].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			storedCats [storedCats.Count - 1].transform.position = transform.position + (Vector3.up * 1.5f) + (Vector3.right*1f);
+			yield return new WaitForSeconds(0.1f);
+			storedCats [storedCats.Count - 1].SetActive (enabled);
+			storedCats [storedCats.Count - 1].GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10f, ForceMode2D.Impulse);
+		} else {
+			storedCats [storedCats.Count - 1].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			storedCats [storedCats.Count - 1].transform.position = transform.position + (Vector3.up * 1.5f) + (Vector3.left*1f);
+			yield return new WaitForSeconds(0.1f);
+			storedCats [storedCats.Count - 1].SetActive (enabled);
+			storedCats [storedCats.Count - 1].GetComponent<Rigidbody2D>().AddForce(Vector2.right * -10f, ForceMode2D.Impulse);
+		}
+		catAddingInstance = StartCoroutine (RemoveBotGato ());
+	}
+
+	void RemoveTopGato(){
+		if (GetComponent<CharacterController2d> ().m_FacingRight) {
+			storedCats [0].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			storedCats [0].transform.position = transform.position + (Vector3.up * 0.75f) + (Vector3.right*0.25f);
+			storedCats [0].SetActive (enabled);
+		} else {
+			storedCats [0].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			storedCats [0].transform.position = transform.position + (Vector3.up * 0.75f) + (Vector3.left*0.25f);
+			storedCats [0].SetActive (enabled);
+		}
+		GameObject.Destroy (heldCats [0].gameObject);
+		//create cat object
+		heldCats.RemoveAt (0);
+		storedCats.RemoveAt (0);
+		catTowerHeight -= catHeight;
+	}
+
+	IEnumerator RemoveBotGato(){
+		GameObject.Destroy (heldCats [heldCats.Count - 1].gameObject);
+		//create cat object
+		heldCats.RemoveAt (heldCats.Count - 1);
+		storedCats.RemoveAt (storedCats.Count - 1);
+		//resort cats
+		for (int i = heldCats.Count - 1; i >= 0; i--) {
+			float elapsedTime = 0f;
+			while (elapsedTime < 1f) {
+				heldCats [i].transform.localPosition = Vector2.Lerp(heldCats [i].transform.localPosition, new Vector2 (0f, catTowerHeight - (catHeight * i)), elapsedTime);
+
+				elapsedTime += Time.deltaTime * 10f;
+				yield return new WaitForEndOfFrame();
+			}
+		}
+		catTowerHeight -= catHeight;
+		catAddingInstance = null;
 	}
 }
