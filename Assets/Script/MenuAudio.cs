de95f0ca3fe9403a45cf0e55b22a8be7ea4e17de
tayusuki <aestrada2@mail.pima.edu>
2018-12-03 19:02:15 -0700

    Added menu sound in game

--- Assets/Script/MenuAudio.cs
+++ Assets/Script/MenuAudio.cs
@@ -1,19 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuAudio : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.anyKey)
-        {
-            GetComponent<AudioSource>().Play();
-        }
-	}
+    public void OnDeselect()
+    {
+        GameObject.Find("ButtonSound").GetComponent<AudioSource>().Play();
+    }
 }
