0db021e531f34ae31999aeba93b95f9793bd89eb
DigiGee <byongbanthom@yahoo.com>
2018-12-03 18:55:25 -0700

    so fetch

---
+++ Assets/Script/MainMenuCongrats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCongrats : MonoBehaviour {
+
+	public AudioClip catsnd;
+
+	void Start () {
+		if (GameManager.beatGame) {
+			StartCoroutine (Play ());
+			GameManager.beatGame = false;
+		}
+	}
+	/*
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.B)) {
+			StartCoroutine (Play ());
+		}
+	}
+	*/
+
+	IEnumerator Play(){
+		GetComponent<AudioSource> ().Play ();
+		GetComponent<ParticleSystem> ().Play ();
+		yield return new WaitForSeconds (13f);
+		GetComponent<ParticleSystem> ().Stop ();
+		yield return new WaitForSeconds (3.5f);
+		GetComponent<AudioSource> ().PlayOneShot (catsnd);
+	}
+}
